e8d57b04db3acdaf7897ba30ccbc21e8bac661b5
jeroen-manders <jeroen.manders@external.mckinsey.com>
2020-02-04 04:44:46 +0100

    Install Packer binary if not present

--- auto-source/packer.sh
+++ auto-source/packer.sh
@@ -5,7 +5,7 @@ function ensure_packer() {
   if [ -f "$filename" ]; then
     log_info "Using Packer version $PACKER_VERSION.";
   else
-    log_info "Installing OPA version $PACKER_VERSION";
+    log_info "Installing Packer version $PACKER_VERSION";
     curl -sSLo "/tmp/packer.zip" https://releases.hashicorp.com/packer/$PACKER_VERSION/packer_${PACKER_VERSION}_linux_amd64.zip;
     cd /tmp && unzip packer.zip;
     mv packer $PACKER
@@ -15,16 +15,31 @@ function ensure_packer() {
 }
 
 function run_aws_packer() {
-	local function_name=run_packer packer_directory ami_name_prefix ami_description parent_ami_prefix vpc_name bastion_name ssh_bastion_username ssh_bastion_private_key_file;
+	local function_name=run_packer packer_directory ami_name_prefix ami_description parent_ami_prefix vpc_name \
+	    bastion_name ssh_bastion_username ssh_bastion_private_key_file security_group_name subnet_name aws_region;
 	import_args "$@";
-	check_required_arguments $function_name packer_directory ami_name_prefix ami_description vpc_name;
-	check_required_variables security_group_name subnet_name aws_region;
+	check_required_arguments $function_name packer_directory ami_name_prefix ami_description vpc_name \
+	    bastion_name ssh_bastion_username ssh_bastion_private_key_file security_group_name subnet_name aws_region;
 
 	local ssh_bastion_host vpc_id;
+	export AWS_DEFAULT_REGION="$aws_region";
 
 	ensure_packer;
+	#vpc_id security_group_id subnet_id ssh_bastion_host;
   get_vpc_id --vpc_name "$vpc_name" --target_variable_name vpc_id;
+  get_security_group_id --vpc_id "$vpc_id" --security_group_name $security_group_name --target_variable_name security_group_id;
+  get_subnet_id --vpc_id "$vpc_id" --subnet_name $subnet_name --target_variable_name subnet_id;
   get_instance_public_dns --instance_name "$bastion_name" --vpc_id "$vpc_id" --target_variable_name ssh_bastion_host;
+
+  log_info "Initializing Packer environment.";
+
+  export vpc_id security_group_id subnet_id ssh_bastion_host ssh_bastion_private_key_file source_ami;
+  log_info "Using bastion $ssh_bastion_host and private key $ssh_bastion_private_key_file";
+
+  if [ ! -f "$ssh_bastion_private_key_file" ]; then
+      log_fatal "Bastion private key file doesn't exist: $ssh_bastion_private_key_file.";
+  fi;
+  log_info "Using vpc '$vpc_id', security group '$security_group_id', subnet '$subnet_id' and bastion '$ssh_bastion_host'.";
 
 	export packer_tmp_dir="/tmp/packer$$";
 	export packer_target_dir="/tmp/packer$$";
@@ -42,7 +57,7 @@ function run_aws_packer() {
             exit 1;
         else
             log_info "Looking for AMI with name starting with '$parent_ami_prefix'";
-            source_ami="$(get_ami --ami_name_prefix "$parent_ami_prefix")";
+            get_ami --ami_name_prefix "$parent_ami_prefix" --target_variable_name source_ami;
         fi;
         if [ -z "$source_ami" -o "$source_ami" == "-null-" -o "$source_ami" == "null" ]; then
             log_error "Unable to find an AMI with name starting with '$parent_ami_prefix'";
@@ -58,24 +73,11 @@ function run_aws_packer() {
 
     [[ "$do_encrypt_boot" == "1" ]] && export encrypt_boot="true" || export encrypt_boot="false";
 
-    log_info "Initializing Packer environment.";
-
-    get_vpc_id --vpc_name "$vpc_name" --target_variable_name vpc_id;
-    get_bastion_public_dns --vpc_id "$vpc_id" --bastion_name "$bastion_name" --target_variable_name ssh_bastion_host;
-    get_security_group_id --vpc_id "$vpc_id" --security_group_name $security_group_name --target_variable_name security_group_id;
-    get_subnet_id --vpc_id "$vpc_id" --subnet_name $subnet_name --target_variable_name subnet_id;
-
-    export vpc_id subnet_id security_group_id ssh_bastion_host ssh_bastion_private_key_file source_ami;
-    log_info "Using bastion $ssh_bastion_host and private key $ssh_bastion_private_key_file";
-    if [ ! -f "$ssh_bastion_private_key_file" ]; then
-        log_fatal "Bastion private key file doesn't exist: $ssh_bastion_private_key_file.";
-    fi;
-    log_info "Using vpc '$vpc_id', security group '$security_group_id', subnet '$subnet_id' and bastion '$ssh_bastion_host'.";
 	oldpwd="$(pwd)";
 
 	cd $packer_tmp_dir
 
-	$packer build $extra_packer_options -machine-readable $json_filename | tee result.out
+	$PACKER build $extra_packer_options -machine-readable $json_filename | tee result.out
 
 	grep 'artifact,0,id' result.out | cut -d, -f6 | cut -d: -f2
 	ami_id="$(grep 'artifact,0,id' result.out | cut -d, -f6 | cut -d: -f2)";
--- packets/Packer AWS build environment/files/shared_create_ami.sh
+++ packets/Packer AWS build environment/files/shared_create_ami.sh
@@ -2,7 +2,13 @@
 
 set -eo pipefail;
 
-vpc_name="$instance.getAttribute("vpc_name")";
+vpc_name="$instance.byVelocity("vpc_velocity_name").getAttribute("vpc_name")";
+security_group_name="$instance.parent.getAttribute("security_group_name")";
+subnet_name="$instance.parent.getAttribute("subnet_name")";
+aws_region="$instance.parent.getAttribute("aws_region")";
+bastion_name="$instance.parent.getAttribute("bastion_name")";
+ssh_bastion_username="$instance.parent.getAttribute("ssh_bastion_username")";
+ssh_bastion_private_key_file="$instance.parent.getAttribute("ssh_bastion_private_key_file")";
 #if ($instance.getAttribute("source_ami") == "")
 parent_ami_prefix="$instance.parent.getAttribute("ami_name_prefix")";
 #else
@@ -10,7 +16,11 @@ parent_ami_prefix="";
 #end
 #[[
 
-run_aws_packer --packer_directory "$default_module_dir/packer" --vpc_name "$vpc_name" \
+run_aws_packer --packer_directory "$default_module_dir/packer" \
+	--vpc_name "$vpc_name" \
+	--security_group_name "$security_group_name" \
+	--subnet_name "$subnet_name" \
+	--aws_region "$aws_region" \
     --ami_name_prefix "$ami_name_prefix" \
     --ami_description "$ami_description" \
     --parent_ami_prefix "$parent_ami_prefix" \
