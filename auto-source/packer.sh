66f4bb49ea1cca70955570d6670229db3f250654
Jeroen Manders <jeroen@manders.be>
2020-08-29 05:26:42 +0000

    Move packer to its own module

--- auto-source/packer.sh
+++ auto-source/packer.sh
@@ -1,25 +1,3 @@
-# This ensure_packer function should be moved to a central location since it exists in aliyun-commons as well
-function ensure_packer() {
-    local packer_version;
-    import_args "$@";
-    check_required_arguments "ensure_packer" packer_version;
-
-    export PACKER="packer-$packer_version";
-    if [ $(which "$PACKER") ]; then
-        log_info "Packer version $packer_version is already installed at $(which $PACKER).";
-    else
-        log_info "Packer version $packer_version not available. Installing it now in the project cache.";
-        curl -Lo "/tmp/packer.zip" https://releases.hashicorp.com/packer/$packer_version/packer_${packer_version}_linux_amd64.zip;
-        cd /tmp;
-        unzip packer.zip;
-        mv packer "/cache/project/bin/$PACKER";
-        chmod u+x "/cache/project/bin/$PACKER";
-        rm -f packer.zip;
-        cd -;
-        log_info "Packer $packer_version is available at $(which $PACKER)";
-    fi;
-}
-
 function run_aws_packer() {
 	local function_name=run_packer packer_directory ami_name_prefix image_owners ami_description source_ami vpc_name \
 	    bastion_name ssh_bastion_username ssh_bastion_private_key_file security_group_name subnet_name aws_region packer_version="1.5.6";
--- packets/Packer binaries/files/ensure_versions_installed.sh
+++
@@ -1,8 +0,0 @@
-log_info "Ensuring that the following versions of Packer are installed:
-$versions";
-
-for version in $versions; do
-	[[ -n "$version" ]] && ensure_packer --packer_version "$version";
-done;
-
-log_info "Installation complete";
